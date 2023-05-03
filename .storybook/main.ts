c63ec78ad8373c28e90709371dd0c43d6d7f8f16
GregVGW <greg.lahaye@vgw.co>
2023-05-03 11:32:29 +1000

    set up pixi storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,14 @@
-import type { StorybookConfig } from "@storybook/html-webpack5";
+import type { StorybookConfig } from "@pixi/storybook-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
   framework: {
-    name: "@storybook/html-webpack5",
+    name: "@pixi/storybook-vite",
     options: {},
   },
   docs: {
